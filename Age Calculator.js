6497815f048dc29cc9c7913ee0119645291ea5e8
klae32 <techie.r13@gmail.com>
2017-05-01 09:13:04 -0600

    This is one of the JS exercises that shows how many days a person has been alive.

---
+++ Age Calculator.js
@@ -0,0 +1,5 @@
+var age =prompt("How old are you?");
+var ageDays = age * 365.25;
+console.log(age + " years is roughly " + ageDays + " days");
+alert(age + " years is roughly " + ageDays + " days");
+
