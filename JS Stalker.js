5cc38022ddaad5ecd3befb20c0f7b66e0e541fbb
klae32 <techie.r13@gmail.com>
2017-04-30 21:24:53 -0600

    I added A JS project and some JS code alongs.

---
+++ JS Stalker.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your First Name?");
+var lastName = prompt("What is your Last Name?");
+var age = prompt("What is your Current Age?");
+
+alert("Welcome to my page " + firstName + lastName + " I am "  + age + " as well");
+console.log("Welcome to my underground page " + firstName + " " + lastName + "I am not actually "  + age + " as well. Just wanted to say that. HAHAHAHAHA");
---
+++ script.js
@@ -0,0 +1,3 @@
+var userName= prompt("What is your name?");
+console.log("Also, great to meert you, " + userName);
+alert("Nice to meert you, " + userName);
