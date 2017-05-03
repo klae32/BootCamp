b164a2561c185f3f4975bd0ff8319a15bcf5f693
klae32 <techie.r13@gmail.com>
2017-05-03 07:01:23 -0600

    Making improvements to the web app Todo List

--- Exercises/todolist.js
+++ Exercises/todolist.js
@@ -5,13 +5,19 @@ var input = prompt("What would you like todo?")
 while(input!== "quit"){
 
 	if(input === "list"){
-	console.log(todos);
+		todos.forEach(function(todo, index){
+	
+	console.log(index + ": " + todos);
+});
 		}
 	else if(input === "new"){
 	var newTodo = prompt("Enter new todo")
 todos.push(newTodo);
-
 	}
+	// else if(input ==="delete"){
+	// var delete = todos.indexOf(input);
+	// var deleted = todos.slice(delete, (delete+1));
+	// }
 	input = prompt("What would you like to do?")
 }
 console.log("OK, YOU QUIT THE APP")
