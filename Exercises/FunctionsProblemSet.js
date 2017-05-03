21392fb3087542a81c06c4d8dbd79a80dc0de8cd
klae32 <techie.r13@gmail.com>
2017-05-02 21:17:49 -0600

    Made changes to file to add 2 different ways to use the function factorial and different way to replace characters in a string.

--- Exercises/FunctionsProblemSet.js
+++ Exercises/FunctionsProblemSet.js
@@ -12,6 +12,25 @@ function factorial(num){
 	}
 	return num *factorial(num-1);
 
+
+}
+
+function factorial2(num){
+	var result = 1;
+	for (i=2; i<= num; i++){
+		result*= i;
+	}
+	return result;
+}
+function factorial3(num){
+	if (num===0){
+		return 1;
+	}
+	var result = num;
+	for(i =num-1; i>= 1; i--){
+		result *=i;
+	}
+	return result
 }
 
 //kebabToSnake
@@ -19,4 +38,6 @@ function factorial(num){
 function kebabToSnake(str){
 var result = str.replace("-", "_");
 return result;
-}
+
+
+//str.replace(/-/g , "_");
