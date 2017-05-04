c2a24c272bf00be7719414c3a95985ec83cd6f78
klae32 <techie.r13@gmail.com>
2017-05-03 18:25:10 -0600

    Working on the different array problem sets and added some function refactoring to the todo assignments.

---
+++ Exercises/ArrayProblemSet.js
@@ -0,0 +1,23 @@
+function printReverse(arr){
+var rev = arr.reverse();
+rev.forEach(function(i){
+console.log(i);
+})
+
+}
+
+function isUniform(arr){
+	for(i=0; i<arr.length; i++){
+		if (arr[0]!==arr[i]){
+			return false;}
+		}
+		return true;
+	}
+function sumArray(arr){
+for(i=0;i<arr.length;i++){
+results = 0;
+
+arr[i] += results
+}
+return results;
+}
--- Exercises/todolist.js
+++ Exercises/todolist.js
@@ -5,19 +5,35 @@ var input = prompt("What would you like todo?")
 while(input!== "quit"){
 
 	if(input === "list"){
-		todos.forEach(function(todo, index){
-	
-	console.log(index + ": " + todos);
-});
+		
+	listTodos();
 		}
 	else if(input === "new"){
-	var newTodo = prompt("Enter new todo")
-todos.push(newTodo);
+	addTodos();
 	}
-	// else if(input ==="delete"){
-	// var delete = todos.indexOf(input);
-	// var deleted = todos.slice(delete, (delete+1));
-	// }
+
+	else if(input ==="delete"){
+	deleteTodo();
+	}
 	input = prompt("What would you like to do?")
 }
 console.log("OK, YOU QUIT THE APP")
+
+function listTodos(){
+	console.log("**********");
+		todos.forEach(function(todo, index){
+		console.log(index + ": " + todos);
+		console.log("**********");})
+	}
+
+function addTodos(){
+var newTodo = prompt("Enter new todo")
+	todos.push(newTodo);
+	var last = todos[(todos.length-1)];
+	console.log(last + " added to list");
+}
+
+function deleteTodo() {
+var index=prompt("Enter index of todo to delete");
+	var deleted=todos.splice(index, 1)
+	console.log(deleted + " has been deleted");}
