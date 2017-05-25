24357b7e87175ec46e68f3abc674a6a6e3074cba
klae32 <techie.r13@gmail.com>
2017-05-24 22:00:10 -0600

    Finished the exercise for my Score Keeper.

--- Exercises/Score Keeper.js
+++ Exercises/Score Keeper.js
@@ -1,20 +1,23 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
+var resetButton= document.getElementById("reset");
 var p1Score = 0;
 var p2Score = 0;
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var numInput = document.querySelector("input");
+var num = document.querySelector("p span");
 var gameOver = false;
 var winningScore = 5;
+
 
 p1Button.addEventListener("click", function(){
 if(!gameOver){
 	p1Score++;
 	if(p1Score=== winningScore){
+		p1Display.classList.add("winner");
 		gameOver=true;
-		if(gameOver===true){
-			p1Display.style.color = "green";
-		}
+		
 	}
 	p1Display.textContent = p1Score;
 	}
@@ -24,11 +27,30 @@ p2Button.addEventListener("click", function(){
 if(!gameOver){
 p2Score++;
 if(p2Score=== winningScore){
+		p2Display.classList.add("winner");
 		gameOver=true;
-		if(gameOver===true){
-			p2Display.style.color = "green";
-		}
+		
 	}
 p2Display.textContent = p2Score;
 }
 });
+
+resetButton.addEventListener("click", function(){
+reset();
+
+});
+numInput.addEventListener("change", function(){
+num.textContent = this.value;
+winningScore = Number(this.value);
+reset();
+});
+
+function reset(){
+p1Score = 0;
+p2Score = 0;
+p1Display.textContent = 0;
+p2Display.textContent = 0;
+p1Display.classList.remove("winner");
+p2Display.classList.remove("winner");
+gameOver = false;
+}
